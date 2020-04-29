a2b941d3b359def22f3f161f4c4cc0d25b205935
Jacob Lewallen <jlewallen@gmail.com>
2020-04-29 11:23:52 -0700

    Split actions into functions.

--- jira-status.go
+++ jira-status.go
@@ -54,6 +54,124 @@ func findEpic(jc *jira.Client, options *Options) (string, error) {
 	return "", fmt.Errorf("unable to find Epic matching '%s'", options.Epic)
 }
 
+func linkEpics(jc *jira.Client, options *Options) error {
+	desiredKey, err := findEpic(jc, options)
+	if err != nil {
+		return err
+	}
+
+	types, _, _ := jc.IssueLinkType.GetList()
+	for _, a := range types {
+		log.Printf("OK: ", a)
+	}
+
+	for _, issueNumber := range flag.Args() {
+		issueKey := fmt.Sprintf("%s-%s", options.Project, issueNumber)
+
+		log.Printf("moving %s to epic %s", issueKey, desiredKey)
+
+		issue, _, err := jc.Issue.Get(issueKey, nil)
+		if err != nil {
+			return fmt.Errorf("error getting issue: %+v", err)
+		}
+
+		linked := false
+
+		for _, link := range issue.Fields.IssueLinks {
+			if link.InwardIssue != nil {
+				if link.InwardIssue.Key == desiredKey {
+					linked = true
+				} else {
+					if link.InwardIssue.Fields.Type.Name == "Epic" {
+						log.Printf("removing (inward) link to: %s", link.InwardIssue.Key)
+						err := deleteLink(jc, link.ID)
+						if err != nil {
+							return fmt.Errorf("error: %v", err)
+						}
+					}
+				}
+			}
+			if link.OutwardIssue != nil {
+				if link.OutwardIssue.Key == desiredKey {
+					linked = true
+				} else {
+					if link.OutwardIssue.Fields.Type.Name == "Epic" {
+						log.Printf("removing (outward) link to: %s", link.OutwardIssue.Key)
+						err := deleteLink(jc, link.ID)
+						if err != nil {
+							return fmt.Errorf("error: %v", err)
+						}
+					}
+				}
+			}
+		}
+
+		if !linked {
+			log.Printf("adding link from %s to %s", issueKey, desiredKey)
+
+			newLink := &jira.IssueLink{
+				Type: jira.IssueLinkType{
+					Name: "Relates",
+				},
+				InwardIssue: &jira.Issue{
+					Key: desiredKey,
+				},
+				OutwardIssue: &jira.Issue{
+					Key: issueKey,
+				},
+			}
+			_, err := jc.Issue.AddLink(newLink)
+			if err != nil {
+				return fmt.Errorf("error: %v", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func displayIssues(jc *jira.Client, options *Options) error {
+	epics, _, err := jc.Issue.Search("type = 'Epic' AND resolution IS EMPTY ORDER BY dueDate DESC", nil)
+	if err != nil {
+		return fmt.Errorf("error getting issues: %+v", err)
+	}
+
+	for _, i := range epics {
+		fmt.Printf("%+v %v (%d linked)\n", i.Key, i.Fields.Summary, len(i.Fields.IssueLinks))
+
+		for _, link := range i.Fields.IssueLinks {
+			if link.InwardIssue != nil {
+				if link.InwardIssue.Fields.Resolution == nil {
+					i := link.InwardIssue
+					if shouldShow(i) {
+						if i.Fields.Assignee != nil {
+							fmt.Printf("  %s %s (%s) (%s)\n", i.Key, i.Fields.Summary, i.Fields.Status.Name, i.Fields.Assignee.Name)
+						} else {
+							fmt.Printf("  %s %s (%s)\n", i.Key, i.Fields.Summary, i.Fields.Status.Name)
+						}
+					}
+				}
+			}
+			if link.OutwardIssue != nil {
+				if link.OutwardIssue.Fields.Resolution == nil {
+					i := link.OutwardIssue
+					if shouldShow(i) {
+						if i.Fields.Assignee != nil {
+							fmt.Printf("  %s %s (%s) (%s)\n", i.Key, i.Fields.Summary, i.Fields.Status.Name, i.Fields.Assignee.Name)
+						} else {
+							fmt.Printf("  %s %s (%s)\n", i.Key, i.Fields.Summary, i.Fields.Status.Name)
+						}
+					}
+				}
+			}
+		}
+
+		fmt.Println()
+	}
+
+	return nil
+}
+
 func main() {
 	options := &Options{}
 	flag.StringVar(&options.Project, "project", "FK", "project prefix")
@@ -72,114 +190,12 @@ func main() {
 	}
 
 	if len(options.Epic) > 0 {
-		desiredKey, err := findEpic(jc, options)
-		if err != nil {
+		if err := linkEpics(jc, options); err != nil {
 			log.Fatalf("error: %v", err)
 		}
-
-		types, _, _ := jc.IssueLinkType.GetList()
-		for _, a := range types {
-			log.Printf("OK: ", a)
-		}
-
-		for _, issueNumber := range flag.Args() {
-			issueKey := fmt.Sprintf("%s-%s", options.Project, issueNumber)
-
-			log.Printf("moving %s to epic %s", issueKey, desiredKey)
-
-			issue, _, err := jc.Issue.Get(issueKey, nil)
-			if err != nil {
-				log.Fatalf("error getting issue: %+v", err)
-			}
-
-			linked := false
-
-			for _, link := range issue.Fields.IssueLinks {
-				if link.InwardIssue != nil {
-					if link.InwardIssue.Key == desiredKey {
-						linked = true
-					} else {
-						if link.InwardIssue.Fields.Type.Name == "Epic" {
-							log.Printf("removing (inward) link to: %s", link.InwardIssue.Key)
-							err := deleteLink(jc, link.ID)
-							if err != nil {
-								log.Fatalf("error: %v", err)
-							}
-						}
-					}
-				}
-				if link.OutwardIssue != nil {
-					if link.OutwardIssue.Key == desiredKey {
-						linked = true
-					} else {
-						if link.OutwardIssue.Fields.Type.Name == "Epic" {
-							log.Printf("removing (outward) link to: %s", link.OutwardIssue.Key)
-							err := deleteLink(jc, link.ID)
-							if err != nil {
-								log.Fatalf("error: %v", err)
-							}
-						}
-					}
-				}
-			}
-
-			if !linked {
-				log.Printf("adding link from %s to %s", issueKey, desiredKey)
-
-				newLink := &jira.IssueLink{
-					Type: jira.IssueLinkType{
-						Name: "Relates",
-					},
-					InwardIssue: &jira.Issue{
-						Key: desiredKey,
-					},
-					OutwardIssue: &jira.Issue{
-						Key: issueKey,
-					},
-				}
-				_, err := jc.Issue.AddLink(newLink)
-				if err != nil {
-					log.Fatalf("error: %v", err)
-				}
-			}
-		}
 	} else {
-		epics, _, err := jc.Issue.Search("type = 'Epic' AND resolution IS EMPTY ORDER BY dueDate DESC", nil)
-		if err != nil {
-			log.Fatalf("error getting issues: %+v", err)
-		}
-
-		for _, i := range epics {
-			fmt.Printf("%+v %v (%d linked)\n", i.Key, i.Fields.Summary, len(i.Fields.IssueLinks))
-
-			for _, link := range i.Fields.IssueLinks {
-				if link.InwardIssue != nil {
-					if link.InwardIssue.Fields.Resolution == nil {
-						i := link.InwardIssue
-						if shouldShow(i) {
-							if i.Fields.Assignee != nil {
-								fmt.Printf("  %s %s (%s) (%s)\n", i.Key, i.Fields.Summary, i.Fields.Status.Name, i.Fields.Assignee.Name)
-							} else {
-								fmt.Printf("  %s %s (%s)\n", i.Key, i.Fields.Summary, i.Fields.Status.Name)
-							}
-						}
-					}
-				}
-				if link.OutwardIssue != nil {
-					if link.OutwardIssue.Fields.Resolution == nil {
-						i := link.OutwardIssue
-						if shouldShow(i) {
-							if i.Fields.Assignee != nil {
-								fmt.Printf("  %s %s (%s) (%s)\n", i.Key, i.Fields.Summary, i.Fields.Status.Name, i.Fields.Assignee.Name)
-							} else {
-								fmt.Printf("  %s %s (%s)\n", i.Key, i.Fields.Summary, i.Fields.Status.Name)
-							}
-						}
-					}
-				}
-			}
-
-			fmt.Println()
+		if err := displayIssues(jc, options); err != nil {
+			log.Fatalf("error: %v", err)
 		}
 	}
 }
